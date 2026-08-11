bdfa87cc2a6ac7bdc8153c4129e0e10c95569222
GANGOUE MBIMI jihrel Gédéon <gedeon.jireh.gangoue23@gmail.com>
2026-08-11 11:40:43 +0100

    fix: champ created_at dans la table journal

--- backend/api/models.py
+++ backend/api/models.py
@@ -266,6 +266,7 @@ class Certificates(models.Model):
 class Journal(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     action = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.action
